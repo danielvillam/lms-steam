71f3adf71cfc3dbd22a7955c8b794ce49cde9ebf
danielvillam <jvillame@unal.edu.co>
2025-01-14 12:27:25 -0500

    Feat: Courses Page

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,13 +1,30 @@
-import Link from "next/link";
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 
-import { Button } from "@/components/ui/button";
+import { db } from '@/lib/db';
 
-const CoursesPage = () => {
+import { DataTable } from './_components/data-table';
+import { columns } from './_components/columns';
+
+const CoursesPage = async () => {
+    const { userId } = await auth();
+
+    if (!userId) {
+        return redirect("/");
+    }
+
+    const courses = await db.course.findMany({
+        where: {
+            userId,
+        },
+        orderBy: {
+           createdAt: "desc"
+        },
+    });
+
   return (
     <div className="p-6">
-      <Link href="/teacher/create">
-        <Button>Nuevo Curso</Button>
-      </Link>
+        <DataTable columns={columns} data={courses} />
     </div>
   );
 };
